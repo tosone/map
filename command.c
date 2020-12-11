39bd8ed0b68af230be0f897f4b5ab1fb1c75dab4
Tosone <i@tosone.cn>
2020-12-11 16:35:40 +0800

    :sparkles: Implement hashmap.

--- command.c
+++ command.c
@@ -8,14 +8,14 @@ Commands Commands_parse(char *cmd, int *len) {
   Commands commands;
   char *token = strtok(cmd, " ");
   while (token != NULL) {
-    if (index == 0) {
+    if (index == 0) { // 虽然这个地方花费的时间会比较多，但是功能是解析人敲的命令，一般不会太长，所以还 OK
       commands = (char **)malloc(sizeof(char *) * (index + 1));
     } else {
       commands = (char **)realloc(commands, sizeof(char *) * (index + 1));
     }
-    size_t size = strlen(token) + 1;
+    size_t size = strlen(token) + 1; // 将要声明字符串本身长度和 '\0' 的空间
     commands[index] = (char *)malloc(sizeof(char) * size);
-    memcpy(commands[index], token, size);
+    memcpy(commands[index], token, size); // 将当前得到的结果复制到新的内存空间中
     token = strtok(NULL, " ");
     index++;
   }
@@ -25,7 +25,7 @@ Commands Commands_parse(char *cmd, int *len) {
 
 void Commands_free(Commands commands, int length) {
   for (int i = 0; i < length; i++) {
-    free(commands[i]);
+    free(commands[i]); // 释放每个字符串的空间
   }
-  free(commands);
+  free(commands); // 释放指针数组的空间
 }
