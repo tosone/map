ceeb39206f9a09c521cd038acf6a5029ac2848fa
Tosone <i@tosone.cn>
2020-12-12 20:58:40 +0800

    :bug: Fix hashmap resize memory leak.

--- hashmap.h
+++ hashmap.h
@@ -5,16 +5,10 @@
 #define HMAP_COMMAND_SET "set"
 #define HMAP_COMMAND_DEL "del"
 
-const HMAP_MAX_LINK_LIST_DEPTH = 8;   // 分支链表的最大深度
-const HMAP_INITIALIZE_SIZE = 8;       // 初始化空间
-const HMAP_GROW_FACTOR = 2;           // 增长因子
-const HMAP_GROW_FACTOR_USABLE = 1024; // 增长因子最大的有效范围
-const HMAP_GROW_MAX = 1024;           // 增长的最大数值
-const HMAP_THRESHOLD = 0.75f;         // 负载因子
-
 typedef struct entry_t {
   char *key;
   void *value;
+  size_t value_length;
   struct entry_t *next;
 } entry_t;
 
@@ -25,9 +19,9 @@ typedef struct {
 } hashmap_t;
 
 unsigned int hash(const char *key);
-hashmap_t *hashmap_create(int size);
+hashmap_t *hashmap_create();
 entry_t *hashmap_pair(const char *key, const void *value, const int value_length);
-void hashmap_set(hashmap_t *hashtable, const char *key, const void *value, const int value_length);
+hashmap_t *hashmap_set(hashmap_t *hashtable, const char *key, const void *value, const int value_length);
 char *hashmap_get(hashmap_t *hashtable, const char *key);
 void hashmap_del(hashmap_t *hashtable, const char *key);
 void hashmap_free(hashmap_t *hashtable);
