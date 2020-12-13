e2a75c54fb856e1a3f510560323c975c413fd491
Tosone <i@tosone.cn>
2020-12-13 21:25:24 +0800

    :sparkles: Dump AVL Tree to dot.

--- avl.h
+++ avl.h
@@ -9,6 +9,7 @@
 #define AVL_COMMAND_IN "in"
 #define AVL_COMMAND_PRE "pre"
 #define AVL_COMMAND_POST "post"
+#define AVL_COMMAND_DUMP "dump"
 
 typedef struct avl_entry_t {
   int key;
@@ -20,7 +21,8 @@ typedef struct avl_entry_t {
 avl_entry_t *avl_create(avl_entry_t *node, int key);
 avl_entry_t *avl_set(avl_entry_t *node, int key);
 bool avl_get(avl_entry_t *root, int key);
-void val_pre_order(avl_entry_t *entry);
-void val_in_order(avl_entry_t *entry);
-void val_post_order(avl_entry_t *entry);
+void avl_pre_order(avl_entry_t *entry);
+void avl_in_order(avl_entry_t *entry);
+void avl_post_order(avl_entry_t *entry);
+void avl_dump(avl_entry_t *entry, char *filename);
 void avl_free(avl_entry_t *entry);
