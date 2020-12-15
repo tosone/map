99eb76597fba88e0a45ac148ca3d0bd6ce474d55
Tosone <i@tosone.cn>
2020-12-15 10:20:54 +0800

    :bug: Fix error print.

--- error.h
+++ error.h
@@ -2,4 +2,5 @@
 
 #define ERR_COMMAND "Invalid command"
 #define ERR_COMMAND_NOT_FOUND "Command not found"
-#define ERR_INTERNAL "Internal error"
+
+#define ERR_INTERNAL(x) printf("[%s] %s:%d %s: %s.\n", __DATE__, __FILE__, __LINE__, "Internal error", x)
