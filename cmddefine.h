38f75125b9ffc8d7658c06316362914d3b680d99
Tosone <i@tosone.cn>
2020-12-20 17:07:23 +0800

    :sparkles: Seprate map funcs.

---
+++ cmddefine.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#define COMMAND_VERSION "version"
+#define COMMAND_HELP "help"
+#define COMMAND_EXIT "exit"
+#define COMMAND_UNAME "uname"
+#define COMMAND_VI "vi"
+#define COMMAND_TCP "tcp"
+
+#define COMMAND_HASH "hash"
+#define COMMAND_HASH_MD5 "md5"
+#define COMMAND_HASH_SHA1 "sha1"
+#define COMMAND_HASH_SHA256 "sha256"
+#define COMMAND_HASH_SHA512 "sha512"
+
+#define COMMAND_BASE64 "base64"
+#define COMMAND_BASE64_DECODE "dec"
+#define COMMAND_BASE64_ENCODE "enc"
+
+#define COMMAND_SERVER "server"
+#define COMMAND_SERVER_START "start"
+#define COMMAND_SERVER_STOP "stop"
+
+#define COMMAND_GAME "game"
+#define COMMAND_GAME_2048 "2048"
